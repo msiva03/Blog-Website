41c96990e183e4782124fec32d35b319f923caca
siva.m <siva.m.cs141@gmail.com>
2026-04-17 17:11:46 +0530

    Authentication part updated

--- register/views.py
+++ register/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse
 from .forms import RegisterForm
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -19,5 +19,17 @@ def register(request):
     return render(request, 'accounts/register.html', {'form': form})
 
 def auth_login(request):
-    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request = request, data= request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username = username, password = password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                homeurl = reverse('home')
+                return HttpResponseRedirect(homeurl)
+    else:
+        form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
