8098ecf749ff88ad5d58a961e1df088d72776b8e
siva.m <siva.m.cs141@gmail.com>
2026-05-03 22:27:42 +0530

    Authentication form update

--- register/forms.py
+++ register/forms.py
@@ -11,3 +11,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'    
--- register/views.py
+++ register/views.py
@@ -8,28 +8,34 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            homeurl = reverse('home')
-            return HttpResponseRedirect(homeurl)
+    if request.user.is_authenticated:
+        return (HttpResponseRedirect('/post/home/'))
     else:
-        form = RegisterForm()        
-    return render(request, 'accounts/register.html', {'form': form})
-
-def auth_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request = request, data= request.POST)
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            user = authenticate(username = username, password = password)
-            print(user)
-            if user is not None:
-                login(request, user)
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
                 homeurl = reverse('home')
                 return HttpResponseRedirect(homeurl)
+        else:
+            form = RegisterForm()        
+        return render(request, 'accounts/register.html', {'form': form})
+
+def auth_login(request):
+    if request.user.is_authenticated:
+        return (HttpResponseRedirect('/post/home/'))
     else:
-        form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
+        if request.method == 'POST':
+            form = AuthenticationForm(request = request, data= request.POST)
+            if form.is_valid():
+                username = form.cleaned_data["username"]
+                password = form.cleaned_data["password"]
+                user = authenticate(username = username, password = password)
+                print(user)
+                if user is not None:
+                    login(request, user)
+                    homeurl = reverse('home')
+                    return HttpResponseRedirect(homeurl)
+        else:
+            form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form': form})
